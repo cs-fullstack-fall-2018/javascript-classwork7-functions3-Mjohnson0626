ac7c347e9759a378284f41f49d00126b3a5ec6d5
Mike <m.g.johnson0626@gmail.com>
2018-07-31 14:34:58 -0500

    final

---
+++ EX18.js
@@ -0,0 +1,17 @@
+function go(){
+    var num1 = 2;
+    var num2 = 2;
+    alert(product(num1, numb));
+    alert(quotient(num1, num2));
+
+}
+function product(numb1, numb2){
+    var multi = numb1*numb2;
+    return multi;
+
+}
+function quotient(numb1, numb2){
+    var div = numb1/numb2;
+    return div;
+}
+go();
---
+++ EX19.js
@@ -0,0 +1,17 @@
+// function go(){
+//     var num1 = prompt("enter a number");
+//     var num2 = prompt("enter a number");
+//     alert(product(num1, numb));
+//     alert(quotient(num1, num2));
+//
+// }
+// function product(numb1, numb2){
+//     var multi = numb1*numb2;
+//     return multi;
+//
+// }
+// function quotient(numb1, numb2){
+//     var div = numb1/numb2;
+//     return div;
+// }
+// go();
